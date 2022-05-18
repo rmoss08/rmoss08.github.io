5e5e47450d577ade3700be8f44fd1a349ba54439
Robert J. Moss <robert.moss3409@gmail.com>
2022-05-18 16:15:21 -0400

    feat: adds select box with styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { QuestionListContext } from "../store/question-list-context";
+import { useContext } from 'react';
+import { QuestionListContext } from '../store/question-list-context';
 import styles from './Form.module.css';
 
 const OPENAI_API_KEY = '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';
@@ -16,7 +16,7 @@ const Form = () => {
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
     };
-  
+
     const response = await fetch(
       'https://api.openai.com/v1/engines/text-curie-001/completions',
       {
@@ -29,16 +29,25 @@ const Form = () => {
       }
     );
     const responseData = await response.json();
-    
+
     return responseData;
   };
-  
+
   const submitHandler = async (event) => {
     event.preventDefault();
-    
-    const prompt = event.target[0].value;
+
+    const selectValue = document.getElementById('question-select').value;
+    const inputValue = event.target[0].value;
+    let prompt;
+
+    if (selectValue !== "No, thanks. I'll ask my own question") {
+      prompt = selectValue;
+    } else {
+      prompt = inputValue;
+    }
+
     const timeStamp = event.timeStamp;
-    
+
     // const responseData = await fetchOpenaiAPI(prompt);
     // const answer = responseData.choices[0].text;
     // const answerFormatted = answer.trim().replace('?', "");
@@ -46,8 +55,8 @@ const Form = () => {
     const question = {
       prompt: prompt,
       answer: 'answerFormatted',
-      timeStamp: timeStamp
-    }
+      timeStamp: timeStamp,
+    };
 
     questionListCtx.addQuestion(question);
   };
@@ -60,11 +69,23 @@ const Form = () => {
       </label>
       <textarea
         id="question-input"
-        rows='5'
-        columns='40'
+        rows="5"
+        columns="40"
         placeholder="Do androids dream of electric sheep?"
         className="border-radius-15"
       />
+      <label htmlFor="question-select">
+        Or choose one of the AI's favourite questions to get the ball rolling.
+      </label>
+      <select id="question-select" className="">
+        <option value="No, thanks. I'll ask my own question">
+          No, thanks. I'll ask my own question
+        </option>
+        <option value="Do androids dream of electric sheep">
+          Do androids dream of electric sheep?
+        </option>
+        <option value="Can you boogie">Can you boogie?</option>
+      </select>
       <button className={styles.ask}>Ask</button>
     </form>
   );
